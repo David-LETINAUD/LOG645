57babce5eb9a4ed5b1d8484f27f47df5ad4204fc
Létinaud <michaelmasson55@gmail.com>
2019-09-27 08:57:07 -0400

    commemtaires

--- par/src/main.c
+++ par/src/main.c
@@ -12,13 +12,16 @@
 int (* solve)(int iterations, int rank, int value) = solveFirst;
 
 int main(int argc, char* argv[]) {
+	// Variables temporelles
     struct timeval timestamp_s;
     struct timeval timestamp_e;
 
+	// Récupération des parametres en argunents
     int problem = atoi(argv[1]);
     int initialValue = atoi(argv[2]);
     int iterations = atoi(argv[3]);
 
+	
     void * solvers[2];
     solvers[0] = solveFirst;
     solvers[1] = solveSecond;
@@ -29,30 +32,40 @@ int main(int argc, char* argv[]) {
 	int world_rank;
     int *rbuf = NULL;
 
+	// Initialiser la bibliothèque MPI
     MPI_Init(NULL, NULL);
+    
+    // Sauvegarde du temps
 	gettimeofday(&timestamp_s, NULL);
 
+	// Obtenir le rang du processus d'appel dans le communicateur
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	
+	// Si correspond au processus parents => allocation du buffer rbuf
 	if (world_rank == 0) {
     	rbuf = malloc(sizeof(int) * DIM);
     }
 	
+	// Appel de la fonction de résolution de la matrice
 	number = solve(iterations, world_rank, initialValue);
 	
+	// Rassemble les valeurs du groupe de processus 
     MPI_Gather(&number, 1, MPI_INT, rbuf, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+	// Si processus parent
     if (world_rank == 0) {
+		// Affichage matrice finale
 		if (problem == 1) {
 			printMatrix1(DIM, rbuf);
 		} else {
 			printMatrix2(DIM, rbuf);
 		}
-		
+		// Calcul et affichage du temps d'execution du processus parent
 		gettimeofday(&timestamp_e, NULL);
 		printRuntime(timestamp_s, timestamp_e);
     }
     
+    // Nettoyer l'environnement d'exécution MPI  
     MPI_Finalize();
     
     return EXIT_SUCCESS;
--- par/src/solver/solver.c
+++ par/src/solver/solver.c
@@ -23,6 +23,7 @@ int solveSecond(int iterations, int rank, int value) {
 		for (int k = 1; k <= iterations; ++k) {
 			usleep(1000);
 			value += rank * k;
+			// Envoyer la valeur calculee à un autre processus (correspondant a la dependance)
 			MPI_Send(&value, 1, MPI_INT, nextRank, 0, MPI_COMM_WORLD);
 		}
 	} else {
@@ -30,12 +31,15 @@ int solveSecond(int iterations, int rank, int value) {
 		int number = 0;
 		
 		for (int k = 1; k <= iterations; ++k) {
+			// Reception de la valeur calculee par le processus precedent (correspondant a la dependance)
 			MPI_Recv(&number, 1, MPI_INT, prevRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			
 			usleep(1000);
 			value += number * k;
 			
+			// Si le processus ne correspond pas a la derniere colonne
 			if (rank < 56) {
+				// Envoyer la valeur calculee à un autre processus (correspondant a la dependance)
 				MPI_Send(&value, 1, MPI_INT, nextRank, 0, MPI_COMM_WORLD);
 			}
 		}
