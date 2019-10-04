425371abb43c070286fc56425031b43045ede39a
Létinaud <michaelmasson55@gmail.com>
2019-10-04 09:31:41 -0400

    execute

--- TP1/par/run.sh
+++ TP1/par/run.sh
@@ -1,20 +1,28 @@
 make all
 
 echo ''
-echo '>>> 1 0 1'
-mpirun -np 64 ./lab1 1 0 1
+echo '>>> 1 8 12'
+mpirun -np 64 ./lab1 1 8 12
 echo '<<<'
 
-echo '>>> 1 5 3'
-mpirun -np 64 ./lab1 1 5 3
+echo '>>> 1 4 5'
+mpirun -np 64 ./lab1 1 4 5
 echo '<<<'
 
-echo '>>> 2 0 1'
-mpirun -np 64 ./lab1 2 0 1
+echo '>>> 1 13 22'
+mpirun -np 64 ./lab1 1 13 22
 echo '<<<'
 
-echo '>>> 2 5 3'
-mpirun -np 64 ./lab1 2 5 3
+echo '>>> 2 0 5'
+mpirun -np 64 ./lab1 2 0 5
+echo '<<<'
+
+echo '>>> 2 2 4'
+mpirun -np 64 ./lab1 2 2 4
+echo '<<<'
+
+echo '>>> 2 7 3'
+mpirun -np 64 ./lab1 2 7 3
 echo '<<<'
 
 make clean
