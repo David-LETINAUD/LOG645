2ebe91b335ba0274b856c5ece1bc50b441a5e871
David-LETINAUD <51359189+David-LETINAUD@users.noreply.github.com>
2019-10-18 10:16:34 -0400

    Update solver.c

--- TP2/src/solver/solver.c
+++ TP2/src/solver/solver.c
@@ -14,10 +14,8 @@ void solveFirst(const int rows, const int cols, const int iterations, const stru
 	#pragma omp parallel for collapse(3)
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			for (int k = 0; k < iterations; k++) {
-				nanosleep(&ts_sleep, NULL);
-				matrix[i][j] += i + j;
-			}
+			nanosleep(&ts_sleep, NULL);
+			matrix[i][j] = (i + j)*iterations;
 		}
 	}
 }
