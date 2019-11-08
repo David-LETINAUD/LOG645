9d826f5e3d17a2cd5cc5c68b4983225c2e6cb9d8
David <davidletinaud@gmail.com>
2019-11-08 00:20:04 -0500

    t

--- TP3/V2/src/output/output.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef OUTPUT_HPP
-#define OUTPUT_HPP
-
-void printMatrix(int rows, int cols, double ** matrix);
-void printStatistics(int threads, long runtime_seq, long runtime_par);
-
-#endif
--- TP3/V2/src/solver/solver.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef SOLVER_HPP
-#define SOLVER_HPP
-
-void solveSeq(int rows, int cols, int iterations, double td, double h, int sleep, double ** matrix);
-void solvePar(int rows, int cols, int iterations, double td, double h, int sleep, double ** matrix);
-
-#endif
